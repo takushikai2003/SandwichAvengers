3c08b2008725d2cd5d690cddef4ed615b8b6ea9b
takushikai <takushikai@gmail.com>
2025-07-02 23:01:47 +0900

    avatar.html OK たぶん

---
+++ env/firebaseCommon.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+      apiKey: "AIzaSyAR8aAj_-vcBgUSft8yH0gAQ81PQogi16w",
+      authDomain: "mbti-2fd58.firebaseapp.com",
+      projectId: "mbti-2fd58",
+      storageBucket: "mbti-2fd58.appspot.com",
+      messagingSenderId: "242270817890",
+      appId: "1:242270817890:web:8061e03390beecc1928b3b"
+};
