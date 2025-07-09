be34c9e1107880dadfc908c4c2f39721caca2798
takushikai <takushikai@gmail.com>
2025-07-09 13:09:12 +0900

    fix

--- TaskManagement/main.js
+++ TaskManagement/main.js
@@ -1,6 +1,8 @@
 import { P_Todo } from "../P_Todo/P_Todo.js";
 import { J_Todo } from "../J_Todo/J_Todo.js";
 
+const mbti = "infj";
+if(mbti.includes("p")){}
 const left_area = document.getElementById("left_area");
 const right_area = document.getElementById("right_area");
 
