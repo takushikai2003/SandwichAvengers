81d58bdca110cc79d764a7c87f9653387b8df646
takushikai <takushikai@gmail.com>
2025-07-09 15:05:04 +0900

    プログレスバーをdateベースに変更

--- progressBar/ProgressBar.js
+++ progressBar/ProgressBar.js
@@ -6,11 +6,15 @@ loadCSS(new URL("./css/style.css", import.meta.url));
 
 export class ProgressBar{
 	/**
-	 * 
-	 * @param {Integer} days 表示する文字数（Sタイプなら7日、Nタイプなら100日）
+	 * @param {Date} startDate 開始日
+	 * @param {Integer} totalDays 表示する文字数（Sタイプなら7日、Nタイプなら100日）
+	 * @property {Integer} totalDays - 総日数（開始日含む）
+	 * @property {Date} startDate - 開始日
 	 */
-	constructor(days){
-		this.totalDays = days;
+	constructor(startDate, totalDays){
+		this.startDate = startDate;
+		this.totalDays = totalDays;
+		this.datesState = new Array(totalDays).fill(false); // 各日の状態を管理する配列(false: 未完了, true: 完了)
 
 		const elem = document.createElement("div");
 		elem.setAttribute("style","width:100%; height:100%;");
@@ -19,9 +23,14 @@ export class ProgressBar{
 		const progressArea = document.createElement("div");
 		progressArea.id = "progress-area";
 		progressArea.innerHTML = ''; // 既存の内容をクリア
-		for (let i = 0; i < days; i++) {
+		for (let i = 0; i < totalDays; i++) {
 			const day = document.createElement('div');
 			day.className = 'calendar-day';
+
+			if(this.datesState[i]){
+				day.classList.add('done');
+			}
+			
 			progressArea.appendChild(day);
 		}
 		elem.appendChild(progressArea);
@@ -34,22 +43,48 @@ export class ProgressBar{
 		elem.appendChild(progressText);
 	}
 
-	completeToday(){
-		// TODO:
-		// 完了したら？
-		// 失敗したら？
+	completeDate(date){
 		const progressArea = document.getElementById('progress-area');
 		const days = progressArea.children;
 
-		let percent = 0;
-		for (let i = 0; i < this.totalDays; i++) {
-			if (!days[i].classList.contains('done')) {
-				days[i].classList.add('done');
-
-				percent = Math.floor(((i+1) / this.totalDays) * 100);
-				break;
-			}
+		// dateとstartDateの差を計算（日）
+		const direction = date.getTime() - this.startDate.getTime();
+		const dayIndex = Math.floor(direction / (1000 * 60 * 60 * 24)); // ミリ秒を日数に変換
+		if (dayIndex < 0 || dayIndex >= this.totalDays) {
+			console.error("指定された日付は範囲外です。");
+			return;
 		}
+		// 完了状態を更新
+		this.datesState[dayIndex] = true;
+		days[dayIndex].classList.add('done'); // 完了状態のクラスを追加
+
+
+		const count = this.datesState.filter(state => state).length; // 完了した日の数
+		const percent = Math.floor((count / this.totalDays) * 100);
+		
+		const progressText = document.getElementById('progress-percent');
+		progressText.textContent = `進捗：${percent}%`;
+	}
+
+	// doneになってるのを解除する
+	incompleteDate(date){
+		const progressArea = document.getElementById('progress-area');
+		const days = progressArea.children;
+
+		// dateとstartDateの差を計算（日）
+		const direction = date.getTime() - this.startDate.getTime();
+		const dayIndex = Math.floor(direction / (1000 * 60 * 60 * 24)); // ミリ秒を日数に変換
+		if (dayIndex < 0 || dayIndex >= this.totalDays) {
+			console.error("指定された日付は範囲外です。");
+			return;
+		}
+		// 完了状態を更新
+		this.datesState[dayIndex] = false;
+		days[dayIndex].classList.remove('done'); // 完了状態のクラスを削除
+		
+		// 完了した日の数をカウント
+		const count = this.datesState.filter(state => state).length; // 完了した日の数
+		const percent = Math.floor((count / this.totalDays) * 100);
 
 		const progressText = document.getElementById('progress-percent');
 		progressText.textContent = `進捗：${percent}%`;
